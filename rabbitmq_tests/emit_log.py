ecc380b374a4c5d3f25a62bce19c3526688f021b
Gilson Paulino <gilsonbp@gmail.com>
2021-08-30 21:07:06 -0300

    Initial Commit

---
+++ rabbitmq_tests/emit_log.py
@@ -0,0 +1,21 @@
+import sys
+import datetime
+import pika
+
+RABBIT_URL = "amqp://guest:guest@localhost:5672/local"
+
+connection = pika.BlockingConnection(pika.URLParameters(RABBIT_URL))
+
+channel = connection.channel()
+
+# Create exchange
+channel.exchange_declare(exchange="logs", exchange_type="fanout")
+
+# Publish on exchange
+message = f"Message {datetime.datetime.now()}"
+
+channel.basic_publish(exchange="stock", routing_key="", body=message)
+
+print(f" [x] Sent: {message}")
+
+connection.close()
---
+++ rabbitmq_tests/receive_logs.py
@@ -0,0 +1,29 @@
+import sys
+
+import pika
+
+RABBIT_URL = "amqp://guest:guest@localhost:5672/local"
+
+connection = pika.BlockingConnection(pika.URLParameters(RABBIT_URL))
+
+channel = connection.channel()
+
+# Create exchange
+channel.exchange_declare(exchange="stock", exchange_type="fanout")
+
+# Create randon queue
+result = channel.queue_declare(queue="stock_process")
+queue_name = result.method.queue
+
+channel.queue_bind(exchange="stock", queue=queue_name)
+
+print(" [*] Waiting for stocks. To exit press CTRL+C")
+
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+channel.start_consuming()
